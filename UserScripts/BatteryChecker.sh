4e9b1205ce68efd250291057e8845d39b11f42d2
Eugeniy Maximov <naotta@gmail.com>
2026-01-17 12:45:05 +0400

    some previos fixes. before fixing for 0.53

--- UserScripts/BatteryChecker.sh
+++ UserScripts/BatteryChecker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-  bat_lvl=$(cat /sys/class/power_supply/BAT1/capacity)
+  bat_lvl=$(cat /sys/class/power_supply/BAT0/capacity)
   if [ "$bat_lvl" -le 15 ]; then
     notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%"
     sleep 600
