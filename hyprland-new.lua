7f28bc0c2b966d8421eefd39205f2c832e370863
Eugeniy Maximov <naotta@gmail.com>
2026-06-12 15:02:50 +0400

    new hyprland config. in progess

---
+++ hyprland-new.lua
@@ -0,0 +1,16 @@
+--local configs = $HOME/.config/hypr/configs
+
+require("configs/Settings.lua")
+require("configs/Keybinds.lua")
+
+--local UserConfigs = $HOME/.config/hypr/UserConfigs
+
+require("UserConfigs/Startup_Apps.lua")
+require("UserConfigs/ENVariables.lua")
+require("UserConfigs/Monitors.lua")
+require("UserConfigs/Laptops.lua")
+require("UserConfigs/LaptopDisplay.lua")
+require("UserConfigs/WindowRules.lua")
+require("UserConfigs/UserKeybinds.lua")
+require("UserConfigs/UserSettings.lua")
+require("UserConfigs/WorkspaceRules.lua")
