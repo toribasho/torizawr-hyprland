8e5bf8fd76db276deed874e79e82eb534bdc2531
Eugeniy Maximov <naotta@gmail.com>
2026-06-12 19:35:05 +0400

    add new main config

---
+++ hyprland.lua
@@ -0,0 +1,14 @@
+-- System settings and variables
+require("configs/Settings")
+-- System keybinds
+require(ConfigsDir .. "/Keybinds")
+
+-- User configurations
+require(UserConfigs .. "/ENVariables")
+--require(UserConfigs .. "/LaptopDisplay")
+--require(UserConfigs .. "/Laptops")
+require(UserConfigs .. "/Monitors")
+require(UserConfigs .. "/Startup_Apps")
+require(UserConfigs .. "/UserKeybinds")
+require(UserConfigs .. "/UserSettings")
+require(UserConfigs .. "/WindowRules")
