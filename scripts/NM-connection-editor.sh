a89214af6807e5697503f61c48119580baa1fa74
Eugeniy Maximov <naotta@gmail.com>
2026-08-24 20:31:58 +0400

    fix argument lengh

--- scripts/NM-connection-editor.sh
+++ scripts/NM-connection-editor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ ! -z $(pgrep nm-connection-editor) ]]; then 
-  pkill -9 nm-connection-editor; 
+if [[ ! -z $(pgrep nm-connection-e) ]]; then 
+  pkill -9 nm-connection-e; 
 else 
   nm-connection-editor; 
 fi
