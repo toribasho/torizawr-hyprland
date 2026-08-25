776c92cc4c83f0f2cc48447e77f37634e4a95f29
Eugeniy Maximov <naotta@gmail.com>
2026-08-25 12:28:04 +0400

    spaceings

--- UserConfigs/UserSettings.lua
+++ UserConfigs/UserSettings.lua
@@ -28,7 +28,7 @@ local baseConfig = {
     },
     decoration = {
         rounding = 10,
-                
+
         active_opacity = 1.0,
         inactive_opacity = 1.0,
         fullscreen_opacity = 1.0,
